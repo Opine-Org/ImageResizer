e8d71d756bca186681add1c0b34a0e4322f9ba15
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-24 11:20:26 -0500

    singleton

--- tests/ImageResizerTest.php
+++ tests/ImageResizerTest.php
@@ -1,11 +1,16 @@
 <?php
 namespace Opine;
+
 use PHPUnit_Framework_TestCase;
+use Opine\Config\Service;
+use Opine\Container\Service;
 
 class ImageResizerTest extends PHPUnit_Framework_TestCase {
+
     public function setup () {
-        $root = __DIR__;
-        $container = new Container($root, $root . '/../container.yml');
+        $root = __DIR__ . '/../public';
+        $config = new Config($root);
+        $container = Container::instance($root, $config, $root . '/../container.yml');
     }
 
     public function testSample () {
